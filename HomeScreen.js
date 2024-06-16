6d9661cd7b50a44449441ac7e1089b344c671f0b
Naresh <naresh.v@gmail.com>
2024-06-16 01:19:15 -0700

    Create HomeScreen.js

---
+++ HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome Home!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+export default HomeScreen;
